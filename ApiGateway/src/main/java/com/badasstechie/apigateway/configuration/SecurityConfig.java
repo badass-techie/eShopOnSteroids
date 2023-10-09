ba4aef83387f884b6c1ccb1274613028bb7c30cd
badass-techie <moses.odhiambo.555@gmail.com>
2023-10-09 14:54:15 +0300

    Catch JWT decode exception and return 401 response with the message

--- ApiGateway/src/main/java/com/badasstechie/apigateway/configuration/SecurityConfig.java
+++ ApiGateway/src/main/java/com/badasstechie/apigateway/configuration/SecurityConfig.java
@@ -3,20 +3,26 @@ package com.badasstechie.apigateway.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtValidationException;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -48,20 +54,27 @@ public class SecurityConfig {
             ServerHttpRequest request = exchange.getRequest();
             String authorizationHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
 
-            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                String token = authorizationHeader.substring(7);
-                Jwt jwt = jwtDecoder.decode(token).share().block();
-                String userId = jwt.getSubject();   // get the subject which is the user id
+            try {
+                if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                    String token = authorizationHeader.substring(7);
+                    Jwt jwt = jwtDecoder.decode(token).share().block();
+                    String userId = jwt.getSubject();   // get the subject which is the user id
 
-                // add the user id as a query param to the request
-                URI uri = UriComponentsBuilder.fromUri(request.getURI())
-                        .queryParam("userId", userId)
-                        .build()
-                        .toUri();
-                request = request.mutate().uri(uri).build();
+                    // add the user id as a query param to the request
+                    URI uri = UriComponentsBuilder.fromUri(request.getURI())
+                            .queryParam("userId", userId)
+                            .build()
+                            .toUri();
+                    request = request.mutate().uri(uri).build();
+                }
+
+                return chain.filter(exchange.mutate().request(request).build());
+            } catch (JwtValidationException jwtValidationException) {
+                ServerHttpResponse response = exchange.getResponse();
+                response.setStatusCode(HttpStatus.UNAUTHORIZED);    // set the status code to 401
+                DataBuffer buffer = response.bufferFactory().wrap(jwtValidationException.getMessage().getBytes(StandardCharsets.UTF_8));
+                return response.writeWith(Mono.just(buffer)); // write the exception message to the response
             }
-
-            return chain.filter(exchange.mutate().request(request).build());
         };
     }
 
